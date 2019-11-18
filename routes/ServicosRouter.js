6aa43f1605cfee26c725dc1351810a7fd83c6aa3
Danilo Rafael <9danilorafael@gmail.com>
2019-11-18 16:52:06 +0000

    Comentei os ServicosDAO que está a dar erro no Heroku

--- routes/ServicosRouter.js
+++ routes/ServicosRouter.js
@@ -1,15 +1,17 @@
 var express = require("express");
-var ServicosDao = require("../models/ServicosDAO");
+//var ServicosDao = require("../models/ServicosDAO");
 var router = express.Router();
 
 router.get('/',function(req,res,next){
-    ServicosDao.getServNome(function(err,result){
+  /*  ServicosDao.getServNome(function(err,result){
         if(err){
             res.status(result.code).json(err);
             return;
         }
         res.status(result.code).send(result.data);
-    },next)
+    },next)*/
+    res.status(200).send({msg:"ok"});
+    
 })
 
 module.exports=router;

841b5d45ab705877bb24579322ab678fc7eb7442
Danilo Rafael <9danilorafael@gmail.com>
2019-11-19 14:36:31 +0000

    alterado ServicosRouter e UtilizadoresRouter porque ainda nao funciona no heroku

--- routes/UtilizadoresRouter.js
+++ routes/UtilizadoresRouter.js
@@ -1,15 +1,17 @@
 var express=require("express");
-var UtilizadoresDao = require("../models/UtilizadoresDAO");
+//var UtilizadoresDao = require("../models/UtilizadoresDAO");
 var router = express.Router();
 
 router.get("/", function(req,res,next){
-    UtilizadoresDao.getUtilizadores(function(err,result){
+    /*UtilizadoresDao.getUtilizadores(function(err,result){
         if(err){
             res.status(result.code).json(err);
             return;
         }
         res.status(result.code).send(result.data);
-    },next)
+    },next)*/
+
+    res.status(200).send({msg:"ok"});
 })
 
 module.exports=router;

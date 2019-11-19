21698362de9d22ad698f73f94a6f6cb1deac96bf
Danilo Rafael <9danilorafael@gmail.com>
2019-11-19 14:28:31 +0000

    adicionado UtilizadoresRouter e alterado ServicosRouter

--- models/ServicosDao.js
+++ models/ServicosDao.js
@@ -5,16 +5,14 @@ module.exports.getServNome=function(callback,next){
         if(err)
             callback(err,{code:500, status:"Erro na conexão da base de dados"})
         
-        else{
-            conn.query("SELECT serv_nome FROM servicos",function(err,results){
+        conn.query("SELECT serv_nome FROM servicos",function(err,results){
             conn.release();
             if(err){
                 callback(err,{code:500,status:"Erro na conexão da base de dados"})
                 return;
             }
             callback(false,{code:200,status:"ok",data:results})
-            })
-        }
+        })
     })
     
 }

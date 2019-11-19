414859b5ccc868d209e5a1e1977cafefe6a805d7
Danilo Rafael <9danilorafael@gmail.com>
2019-11-19 17:17:18 +0000

    alterado ServicosRouter e comentado UtilizadoresRouter para verificar erro no heroku

--- routes/ServicosRouter.js
+++ routes/ServicosRouter.js
@@ -1,5 +1,6 @@
 var express = require("express");
 //var ServicosDao = require("../models/ServicosDAO");
+var ServicosDao = require("../models/ServicosDAO");
 var router = express.Router();
 
 router.get('/',function(req,res,next){
@@ -10,8 +11,16 @@ router.get('/',function(req,res,next){
         }
         res.status(result.code).send(result.data);
     },next)*/
+
+    ServicosDao.getServNome(function(err,result){
+        if(err){
+            res.status(result.code).json(err);
+            return;
+        }
+        res.status(result.code).send(result.data);
+    },next)
     
-    res.status(200).send({msg:"ok"});
+    //res.status(200).send({msg:"ok"});
     
 })
 

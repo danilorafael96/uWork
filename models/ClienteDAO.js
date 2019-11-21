5ab618af31033fd67c9d6cec675abbf4ef17658e
Fábio Renato <fabiorenato393@gmail.com>
2019-11-21 02:56:36 +0000

    Ficheiro ClienteDAO actualizado

--- models/ClienteDAO.js
+++ models/ClienteDAO.js
@@ -19,24 +19,3 @@ module.exports.getClienteId = function(callback, next) {
 
     })
 }
-
-
-module.exports.getCliente = function (id, callback, next) {
-    pool.getConnection(function(err,conn){
-        if (err) {
-            callback(err,{code: 500, status: "Erro na conexão da base de dados"})
-        }
-
-        // ? is the place where we will replace for the parameter
-        // This is a prepared statement, so that we avoid SQL injection
-        conn.query("select cliserv_id from pts where id = ?", [id], function(err, results) {
-           conn.release();
-            if (err) {
-                callback(err,{code: 500, status: "Erro na conexão da base de dados"})
-                return;
-            } 
-            // sending only the first since there is only one (id is primary key)
-            callback(false, {code: 200, status:"ok", data: results[0]})
-        })
-    })
-}

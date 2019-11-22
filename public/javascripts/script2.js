d1a3cee23cc8d8591fce01b029a0ce64b4a761dc
Danilo Rafael <9danilorafael@gmail.com>
2019-11-22 08:44:14 +0000

    filtro Tipos de serviço já funciona

--- public/javascripts/script2.js
+++ public/javascripts/script2.js
@@ -9,7 +9,7 @@ window.onload=function(){
 		method:"get",
 		contentType:"aplication/json",
 		dataType:"json",
-		sucess: function(res,status,jqXHR){
+		success: function(res,status,jqXHR){
 			console.log(status);
 			if(res.err){
 				console.log(JSON.stringify(res));
